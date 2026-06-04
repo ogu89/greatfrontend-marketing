f6811333f5e7ddb193276decc126a5da0d0cc75d
ogu89 <kawakami.a@ctw.inc>
2026-06-04 16:38:47 +0900

    feat: change showing different components based on url

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { ProfileCard } from "./challenges/profile-card";
-// import { TestimonialCard } from "./challenges/testimonial-card";
+import { TestimonialCard } from "./challenges/testimonial-card";
 
 function App() {
+  const path = window.location.pathname;
+
+  let challenge = <></>;
+
+  if (path === "/testimonial-card") {
+    challenge = <TestimonialCard />;
+  }
+
+  if (path === "/profile-card") {
+    challenge = <ProfileCard />;
+  }
+
   return (
-    <main className="flex min-h-screen w-screen justify-center bg-[linear-gradient(147.52deg,#f9fafb_8.89%,#d2d6db_100.48%)]">
-      {/* <TestimonialCard /> */}
-      <ProfileCard />
+    <main className="flex min-h-screen w-screen items-center justify-center bg-[linear-gradient(147.52deg,#f9fafb_8.89%,#d2d6db_100.48%)] p-6">
+      {challenge}
     </main>
   );
 }
