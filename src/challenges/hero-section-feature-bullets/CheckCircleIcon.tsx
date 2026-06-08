dd35360dcf746221d5dc5e5c812b23fe696536ee
ogu89 <kawakami.a@ctw.inc>
2026-06-08 17:36:41 +0900

    fix: clena up

--- src/challenges/hero-section-feature-bullets/CheckCircleIcon.tsx
+++ src/challenges/hero-section-feature-bullets/CheckCircleIcon.tsx
@@ -1,10 +1,10 @@
 export function CheckCircleIcon() {
   return (
-    <svg className="size-6 shrink-0" viewBox="0 0 20 20" aria-hidden="true">
-      <circle cx="10" cy="10" r="10" className="fill-indigo-50" />
+    <svg className="size-6 shrink-0" viewBox="0 0 24 24" aria-hidden="true">
+      <circle cx="12" cy="12" r="12" className="fill-indigo-50" />
       <path
-        d="M14.03 7.22a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06L9 11.19l3.97-3.97a.75.75 0 0 1 1.06 0Z"
-        className="fill-indigo-700"
+        d="M9.65 15.52 5.05 10.93l-.71.71L10 17.29l9.9-9.9-.71-.71L10 15.88l-.35-.36Z"
+        className="fill-indigo-500"
       />
     </svg>
   );
--- src/challenges/hero-section-feature-bullets/HelloSectionFeatureBullets.tsx
+++ src/challenges/hero-section-feature-bullets/HelloSectionFeatureBullets.tsx
@@ -1,58 +1,56 @@
 import { CheckCircleIcon } from "./CheckCircleIcon";
 
+const features = [
+  "Minimum 5K image resolution",
+
+  "Various format variants available",
+
+  "Retina display support",
+];
+
 export function HelloSectionFeatureBullets() {
   // Write custom JavaScript here.
   // You may ignore this file and delete if if JavaScript is not required for your challenge.
 
   return (
     <>
-      <div className="flex w-full h-auto  bg-white m-1 rounded-lg shadow-lg">
-        <div className="lg:px-20 lg:py-30 py-12 px-5 grid lg:grid-cols-2">
-          <div className="flex flex-col gap-y-14">
-            {/* title */}
-            <span className="font-medium lg:text-7xl sm:text-5xl text-3xl">
-              Premium abstract images
-            </span>
-            {/* check lists */}
-            <ul className="flex flex-col gap-4">
-              <li className="flex items-center gap-2">
-                <CheckCircleIcon />{" "}
-                <span className="text-xl text-gray-500">
-                  Minimum 5K image resolution
-                </span>
-              </li>
-              <li className="flex items-center gap-2">
+      <section className="mx-auto grid max-w-[1440px] bg-white gap-12 rounded-md bg-gradient-to-b  px-4 py-[62px] shadow-lg md:grid-cols-6 md:gap-8 md:py-[72px] lg:grid-cols-12 lg:px-24 lg:py-[105px]">
+        <div className="flex flex-col justify-center gap-8 md:col-span-6 md:gap-16 lg:col-span-5">
+          {/* title */}
+          <h1 className="text-4xl font-semibold leading-10 text-neutral-900 sm:text-5xl sm:leading-none lg:text-6xl lg:leading-none">
+            Premium abstract images
+          </h1>
+          {/* check lists */}
+          <ul className="flex flex-col gap-5 text-lg leading-7 text-neutral-600">
+            {features.map((feature) => (
+              <li key={feature} className="flex items-center gap-3">
                 <CheckCircleIcon />
-                <span className="text-xl text-gray-500">
-                  Various format variants available
-                </span>
+                <span>{feature}</span>
               </li>
-              <li className="flex items-center gap-2">
-                <CheckCircleIcon />{" "}
-                <span className="text-xl text-gray-500">
-                  Retina display support{" "}
-                </span>
-              </li>
-            </ul>
-            {/* buttons */}
-            <div className="flex gap-6 w-full">
-              <button className="px-6 w-40 shadow-sm h-12 rounded-sm hover:cursor-pointer">
-                Learn more
-              </button>
-              <button className="px-6 w-40 shadow-sm h-12 bg-indigo-700 text-white rounded-sm hover:cursor-pointer">
-                See pricing
-              </button>
-            </div>
-          </div>
-          <div>
-            <img
-              className="w-full rounded-sm"
-              src="images/hero-section-feature-bullets/prism.png"
-              alt="hero image"
-            />
+            ))}
+          </ul>
+          {/* buttons */}
+          <div className="flex flex-row-reverse gap-4 sm:flex-row sm:justify-start sm:gap-8">
+            <button className="h-12 flex-1 rounded border border-neutral-200 bg-white px-5 font-medium text-neutral-900 shadow-sm hover:bg-neutral-50 focus:outline-none focus:ring-4 focus:ring-indigo-100 disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none sm:h-[60px] sm:w-[213px] sm:flex-none sm:text-lg lg:w-[175.5px]">
+              Learn more
+            </button>
+            <button className="h-12 flex-1 rounded bg-indigo-700 px-5 font-medium text-white shadow-sm hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-100 disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none sm:h-[60px] sm:w-[213px] sm:flex-none sm:text-lg lg:w-[175.5px]">
+              See pricing
+            </button>
           </div>
         </div>
-      </div>
+
+        {/* <img
+            className="w-full rounded-sm"
+            src="images/hero-section-feature-bullets/prism.png"
+            alt="hero image"
+          /> */}
+        <img
+          className="h-[264px] w-full rounded object-cover md:col-span-6 md:h-[526px] lg:col-span-7 lg:h-full"
+          src="images/hero-section-feature-bullets/prism.png"
+          alt="hero image"
+        />
+      </section>
     </>
   );
 }
